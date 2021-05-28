d6a2cf84b7445950d416caba687820450c085303
EXT02D19145 <ilhan.ekiz@consultant.turkcell.com.tr>
2021-05-28 20:26:30 +0300

    restaurant detail page created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-class App extends React.Component {
+import React, { Component } from 'react';
+import './App.scss';
+
+import RestaurantDetail from './container/RestaurantDetail/restaurantDetail';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+class App extends Component {
  render() {
   return(
-   <div>
-    My App Component
-   </div>
+    <Router>
+        <Switch>
+          <Route path="/">
+            <RestaurantDetail />
+          </Route>
+        </Switch>
+    </Router>
   );
  }
 }
---
+++ src/container/RestaurantDetail/restaurantDetail.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class RestaurantDetail extends Component {
+ render() {
+  return(
+    <div>Restaurant Detail Page</div>
+  );
+ }
+}
+export default RestaurantDetail
