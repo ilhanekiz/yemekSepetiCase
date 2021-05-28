43ed1af91b8d5130bf20f550218845f43d253d39
EXT02D19145 <ilhan.ekiz@consultant.turkcell.com.tr>
2021-05-28 22:18:26 +0300

    header added.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.scss';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RestaurantDetail from './container/RestaurantDetail/restaurantDetail';
+import Header from './components/Header/Header';
 
 const App = () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/">
         <RestaurantDetail />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <header id="header">
+    <div className="container">
+      <img className="logo" src="src/assets/images/yemeksepeti.png" alt="Yemek Sepeti Logo" />
+    </div>
+  </header>
+);
+
+export default Header;
